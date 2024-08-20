d71d7939c1aa72375a2aa356e3d3931f628ff94b
Tebogo Nomnqa <tebzax2@gmail.com>
2024-08-20 19:28:11 +0200

    level sector button on home working (#3)
    * level sector button on home working
    
    * its fine
    
    ---------
    
    Co-authored-by: siphesihle1415 <156804322+siphesihle1415@users.noreply.github.com>

--- scripts/home_scene.gd
+++ scripts/home_scene.gd
@@ -1,18 +1,26 @@
 extends VBoxContainer
 
 @onready var play_button: TextureButton = $Top/CenterContainer/PlayButton
+@onready var level_selector_button: TextureButton = $Bottom/LevelSelectorButton
 @onready var volume_button: TextureButton = $Bottom/VolumeButton
 
-const SOUND_ON = 'res://assets/GUI/Button/Icon/SoundOn.png'
-const SOUND_OFF = 'res://assets/GUI/Button/Icon/SoundOff.png'
+const SOUND_ON = 'res://assets/gui/Button/Icon/SoundOn.png'
+const SOUND_OFF = 'res://assets/gui/Button/Icon/SoundOff.png'
 
 var loading_scene = preload("res://scenes/loading.tscn").instantiate()
+var level_selector_scene = preload("res://scenes/LevelMenu/LevelScreen.tscn").instantiate()
 
 func _ready():
 	play_button.pressed.connect(load_loading_scene)
 	volume_button.pressed.connect(toggle_volume)
+	level_selector_button.pressed.connect(load_level_selector_scene)
 
+func load_level_selector_scene():
+	get_tree().current_scene.queue_free()
+	get_tree().root.add_child(level_selector_scene)
+	
 func load_loading_scene():
+	get_tree().current_scene.queue_free()
 	get_tree().root.add_child(loading_scene)
 
 func toggle_volume():
