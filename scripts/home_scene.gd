da53f2ae59ec9455744e5ff56a27b3f6b5580b9b
Farisani-Ratshikombo-BBD <farisanipaul.ratshikombo@vcontractor.co.za>
2024-08-21 18:40:38 +0200

    Adding sounds

--- scripts/home_scene.gd
+++ scripts/home_scene.gd
@@ -3,6 +3,7 @@ extends VBoxContainer
 @onready var play_button: TextureButton = $Top/CenterContainer/PlayButton
 @onready var level_selector_button: TextureButton = $Bottom/LevelSelectorButton
 @onready var volume_button: TextureButton = $Bottom/VolumeButton
+@onready var home_sound = $HomeScreenSound
 
 const SOUND_ON = 'res://assets/gui/Button/Icon/SoundOn.png'
 const SOUND_OFF = 'res://assets/gui/Button/Icon/SoundOff.png'
@@ -11,6 +12,7 @@ var loading_scene = preload("res://scenes/loading.tscn").instantiate()
 var level_selector_scene = preload("res://scenes/LevelMenu/LevelSelector.tscn").instantiate()
 
 func _ready():
+	home_sound.play()
 	play_button.pressed.connect(load_loading_scene)
 	volume_button.pressed.connect(toggle_volume)
 	level_selector_button.pressed.connect(load_level_selector_scene)
@@ -26,5 +28,7 @@ func load_loading_scene():
 func toggle_volume():
 	if (volume_button.texture_file == SOUND_ON):
 		volume_button.texture_file = SOUND_OFF
+		home_sound.play()
 	else:
 		volume_button.texture_file = SOUND_ON
+		home_sound.stop()
--- scripts/loading.gd
+++ scripts/loading.gd
@@ -1,6 +1,7 @@
 extends VBoxContainer
 
 @onready var progress_bar: TextureProgressBar = $ProgressBar
-
+@onready var gamePlaySound = $LoadingSound
+	
 func _on_timer_timeout():
 	progress_bar.value += 1
