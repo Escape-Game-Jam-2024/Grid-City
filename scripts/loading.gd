99be5ba69bdc9ebcbdc3ca9613ff5fb1096ed292
Farisani Paul Ratshikombo <farisani.ratshikombo@bbd.co.za>
2024-08-25 01:52:58 +0200

    Latest Music Update

--- scripts/loading.gd
+++ scripts/loading.gd
@@ -4,15 +4,20 @@ signal timeout
 
 @onready var progress_bar: TextureProgressBar = $ProgressBar
 @onready var timer: Timer = $Timer
+@onready var progressSound: AudioStreamPlayer2D = $ProgressSound
 
 const DELAY: float = 0.2
 
 var time_elapsed: float = 0
 
+func _ready():
+	progressSound.play()
+	
 func _on_timer_timeout():
 	if time_elapsed >= DELAY:
 		timeout.emit()
 		timer.stop()
 		
+	#progressSound.play()
 	progress_bar.value += 1
 	time_elapsed += timer.wait_time
