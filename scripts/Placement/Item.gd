d2744d5df855c5bbadce38c3fcd924cb7333fb65
Tebogo Nomnqa <tebzax2@gmail.com>
2024-08-24 20:02:40 +0200

    Pole placement improvements (#11)
    * place pole on pavement only and cancel placement with right click
    
    * remove placed pole using right click
    
    * remove unused function
    
    * fix ui

--- scripts/Placement/Item.gd
+++ scripts/Placement/Item.gd
@@ -3,29 +3,54 @@ extends Sprite2D
 @onready var area_2d: Area2D = $Area2D
 @onready var ok: ColorRect = $Ok
 @onready var deny: ColorRect = $Deny
+enum Layers {
+	GROUND,
+	GRASS,
+	ROAD,
+	PAVEMENT,
+	HOUSE,
+}
+var layerOn
 
-func _process(_delta):
-	var mouse_tile = GameManager.tilemap.local_to_map(get_global_mouse_position())
-	var local_pos = GameManager.tilemap.map_to_local(mouse_tile)
-	var world_pos = GameManager.tilemap.to_global(local_pos)
-	global_position = world_pos + Vector2(959, 540)
-	
-	if $Area2D.get_overlapping_areas().size() > 0:
-		$Deny.show()
-		$Ok.hide()
+func _process(delta):
+	var mouse_tile: Vector2i = GameManager.tilemap.local_to_map(get_global_mouse_position())
+	var local_pos: Vector2 = GameManager.tilemap.map_to_local(mouse_tile)
+	var world_pos: Vector2 = GameManager.tilemap.to_global(local_pos)
+	global_position = world_pos
+	layerOn = GameManager.cityLayout[mouse_tile.x][mouse_tile.y]
+	if (area_2d.get_overlapping_areas().size() > 0 ) || !(layerOn == Layers.PAVEMENT) :
+		deny.show()
+		ok.hide()
 	else:
-		$Ok.show()
-		$Deny.hide()
+		ok.show()
+		deny.hide()
 
 func _unhandled_input(event):
+	# place item
 	if event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
 		var arr = area_2d.get_overlapping_areas()
 		for obj in arr:
 			obj.get_parent().queue_free()
 		
+		if !( (area_2d.get_overlapping_areas().size() > 0 ) || !(layerOn == Layers.PAVEMENT) ):
+			set_process(false)
+			set_process_unhandled_input(false)
+			area_2d.monitoring = false
+		
+		ok.hide()
+		deny.hide()
+	# unselect item
+	if event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_RIGHT:
 		set_process(false)
 		set_process_unhandled_input(false)
 		area_2d.monitoring = false
-		
-		$Ok.hide()
-		$Deny.hide()
+		ok.hide()
+		deny.hide()
+		GameManager.emit_signal("item_unselected",self)
+	
+	
+
+func _on_area_2d_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+		GameManager.emit_signal("item_unselected",self)
+
