c29f2a141dc92fd04cdbcc16a0e34a5dfaa67f2f
tebzatheman <tebzax2@gmail.com>
2024-08-24 10:43:34 +0200

    show red on overlap and green when its fine

--- scripts/Placement/Item.gd
+++ scripts/Placement/Item.gd
@@ -1,21 +1,31 @@
 extends Sprite2D
 
-func _ready():
-	pass
+@onready var area_2d: Area2D = $Area2D
+@onready var ok: ColorRect = $Ok
+@onready var deny: ColorRect = $Deny
 
 func _process(delta):
 	var mouse_tile = GameManager.tilemap.local_to_map(get_global_mouse_position())
 	var local_pos = GameManager.tilemap.map_to_local(mouse_tile)
 	var world_pos = GameManager.tilemap.to_global(local_pos)
 	global_position = world_pos
+	if $Area2D.get_overlapping_areas().size() > 0:
+		$Deny.show()
+		$Ok.hide()
+	else:
+		$Ok.show()
+		$Deny.hide()
 
 func _unhandled_input(event):
 	if event is InputEventMouseButton && event.pressed && event.button_index == MOUSE_BUTTON_LEFT:
-		var arr = $Area2D.get_overlapping_areas()
+		var arr = area_2d.get_overlapping_areas()
 		for obj in arr:
 			obj.get_parent().queue_free()
 		
 		set_process(false)
 		set_process_unhandled_input(false)
-		$Area2D.monitoring = false
+		area_2d.monitoring = false
+		
+		$Ok.hide()
+		$Deny.hide()
 
